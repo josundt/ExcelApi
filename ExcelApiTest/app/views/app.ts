7803e10dde46952290409e63101f0693deaf2262
Jørn Andre Sundt <jorn.andre.sundt@jasmin.no>
2015-05-17 20:34:39 +0200

    A few missing files from previous commit

---
+++ ExcelApiTest/app/views/app.ts
@@ -0,0 +1,17 @@
+﻿import {Router, RouterConfiguration} from 'aurelia-router';
+
+export class App {
+    router: Router;
+
+    configureRouter(config: RouterConfiguration, router: Router) {
+        config.title = 'Aurelia';
+        config.map([
+            { route: ['', 'home'], moduleId: './home', nav: true, title: 'Excel OData WebApi' },
+            { route: 'flickr', moduleId: './flickr', nav: true },
+            { route: 'esri-map', moduleId: './esri-map', nav: true, title: 'ESRI Map' },
+            { route: 'child-router', moduleId: './child-router', nav: true, title: 'Child Router' }
+        ]);
+
+        this.router = router;
+    }
+}
---
+++ ExcelApiTest/typings/tsd.d.ts
@@ -0,0 +1,3 @@
+/// <reference path="requirejs/require.d.ts" />
+/// <reference path="bootstrap/bootstrap.d.ts" />
+/// <reference path="jquery/jquery.d.ts" />
